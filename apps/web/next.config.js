72bce254449663d0234f17aa5a48e0d7d47032cf
Jesse A. Johnson <jesseajohnson@pm.me>
2022-11-13 16:35:15 -0500

    first commit

---
+++ apps/web/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  distDir: '../../dist'
+}
+
+module.exports = nextConfig
